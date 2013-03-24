ce60df60f38fd17ed1e36db257e18f3b7cf949df
Pivotal Labs <pivotal@pivotals-imac.home>
2013-03-24 20:13:52 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoactions::Application.config.secret_token = 'c446d213f0a35ae0e41d6f01460205f42409af72dd0db8556294009389b58784be8c1a81ae77335151bc422c676dcdc55a9a21e50bfccd9d7279e85b39639543'
